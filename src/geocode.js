5974e2712952051289fa9cdcbbbc08375dd74a1c
rcspam <rcspam@users.noreply.github.com>
2019-06-22 11:21:48 +0200

    Update geocode.js

--- src/geocode.js
+++ src/geocode.js
@@ -1,7 +1,7 @@
 const { memoize } = require('cerebro-tools')
 
 const geocode = (term, userLang) => {
-  const url = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(term)}&language=${userLang}`
+  const url = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(term)}&language=${userLang}&key=AIzaSyDqXQ-ENWbbjxE0pLPiS3fv7qJStrCVp60`
   return fetch(url)
     .then(response => response.json())
     .then(json => json.results)
